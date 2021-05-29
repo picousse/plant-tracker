1405d5d27f469a2b4a20fbc3a8c02b365b3063ea
rpi4 planter <planter@plant.er>
2021-05-29 07:32:49 +0100

    F

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # plant-tracker
+
+## Scan for devices
+```
+sudo hcitool lescan
+```
+
+
+## Influxdb
+
+
+```
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/os-release
+echo "deb https://repos.influxdata.com/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+```
+
+```
+sudo apt update && sudo apt install -y influxdb
+```
+
+```
+sudo systemctl unmask influxdb.service
+sudo systemctl start influxdb
+sudo systemctl enable influxdb.service
+```
+
+```
+create database home
+use home
+
+create user grafana with password '<passwordhere>' with all privileges
+grant all privileges on home to grafana
+
+show users
+
+user admin
+---- -----
+grafana true
+
+```
+
+
+## Grafana
+
+```
+
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
+```
+
+```
+sudo apt update && sudo apt install -y grafana
+```
+
+```
+sudo systemctl unmask grafana-server.service
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server.service
+
+```
+
+
+
+https://simonhearne.com/2020/pi-influx-grafana/
---
+++ query_data.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+from btlewrap.bluepy import BluepyBackend
+# from btlewrap import BluepyBackend
+
+from miflora.miflora_poller import MiFloraPoller
+
+from miflora.miflora_poller import (
+    MI_BATTERY,
+    MI_CONDUCTIVITY,
+    MI_LIGHT,
+    MI_MOISTURE,
+    MI_TEMPERATURE,
+    MiFloraPoller,
+)
+
+from influxdb import InfluxDBClient
+
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+def poll(antenna):
+    """Poll data from the sensor."""
+    # backend = _get_backend(args)
+    poller = MiFloraPoller(antenna[1], BluepyBackend, cache_timeout=60)
+    # print("Getting data from Mi Flora")
+    # print(f"FW: {poller.firmware_version()}")
+    # print(f"Name: {poller.name()}")
+    # print("Temperature: {}".format(poller.parameter_value(MI_TEMPERATURE)))
+    # print("Moisture: {}".format(poller.parameter_value(MI_MOISTURE)))
+    # print("Light: {}".format(poller.parameter_value(MI_LIGHT)))
+    # print("Conductivity: {}".format(poller.parameter_value(MI_CONDUCTIVITY)))
+    # print("Battery: {}".format(poller.parameter_value(MI_BATTERY)))
+
+    json_body = [
+        {
+            "measurement": antenna[0],
+            "fields":{
+                "temperature": poller.parameter_value(MI_TEMPERATURE),
+                "moisture": poller.parameter_value(MI_MOISTURE),
+                "light": poller.parameter_value(MI_LIGHT),
+                "conductivity": poller.parameter_value(MI_CONDUCTIVITY),
+                "battery": poller.parameter_value(MI_BATTERY)
+            }
+        }
+    ]
+
+    print(json_body)
+    return json_body
+
+def write_to_influx(json_body):
+    
+    client = InfluxDBClient(host=config['Influx']['host'], port=int(config['Influx']['port'])
+
+    client.write_points(
+        json_body,
+        database = "home")
+
+if __name__ == "__main__":
+    for i in config['Connector']:
+        try: 
+            body = poll([i, config['Connector'][i]])
+            write_to_influx(body)
+        except:
+            pass
+
+
